d7ad1f906558d235d0056bdfe63467d2e53d2681
Aro5683 <arotesting54@gmail.com>
2026-02-17 05:38:26 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,23 @@
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open("edu-cache").then(cache=>{
-return cache.addAll([
-"index.html",
-"master.bedx"
-]);
-})
-);
+const CACHE_NAME="edu-cache-v1";
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll([
+        "/miniature-spork/",
+        "/miniature-spork/index.html",
+        "/miniature-spork/master.bedx",
+        "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-);
+self.addEventListener("fetch",event=>{
+  event.respondWith(
+    caches.match(event.request).then(response=>{
+      return response || fetch(event.request);
+    })
+  );
 });
