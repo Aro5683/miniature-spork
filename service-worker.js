f59ed9b422c5b7f280eca6a17a55bc8b066755b6
Aro5683 <arotesting54@gmail.com>
2026-02-17 17:30:14 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,37 @@
-const CACHE_NAME="edu-cache-v1";
+const CACHE_NAME="edu-cache-v2"; // increase version when updating
 
 self.addEventListener("install",event=>{
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll([
-        "/miniature-spork/",
-        "/miniature-spork/index.html",
-        "/miniature-spork/master.bedx",
-        "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js"
-      ]);
-    })
-  );
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll([
+"./",
+"./index.html",
+"./manifest.json"
+]);
+})
+);
+self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+event.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.map(key=>{
+if(key!==CACHE_NAME){
+return caches.delete(key);
+}
+})
+);
+})
+);
+self.clients.claim();
 });
 
 self.addEventListener("fetch",event=>{
-  event.respondWith(
-    caches.match(event.request).then(response=>{
-      return response || fetch(event.request);
-    })
-  );
+event.respondWith(
+caches.match(event.request).then(response=>{
+return response||fetch(event.request);
+})
+);
 });
